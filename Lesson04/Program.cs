68cf3950aa4b7991460b86a5ab84b4d1a262539d
Your Name <yakir.israel1@gmail.com>
2026-05-31 19:10:45 +0300

    functions, out, ref, recursion

--- Lesson04/Program.cs
+++ Lesson04/Program.cs
@@ -1,3 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string UserName = "yakir";
+string Password = "12345";
+//GreetUser(UserName);
+
+UserName = "Adi";
+//GreetUser(Password);
+
+//Console.WriteLine(Factorial(5));
+SpawnEnemiesRecusivly(3);
+
+
+
+
+void GreetUser(string name)
+{
+    Console.WriteLine("Hello " +  name);
+}
+
+int Factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    
+    return n * Factorial(n - 1);
+}
+
+void SpawnEnemiesRecusivly(int numberOfEnemies)
+{
+    if (numberOfEnemies == 0)
+        return;
+    
+    Console.WriteLine("Spawning enemy " +  numberOfEnemies);
+    SpawnEnemiesRecusivly(numberOfEnemies - 1);
+}
